82dcb8d43b0cd29b0005a7a4d1c9d79bde90a73a
Rich McGhee <rmcghee@stowers.org>
2024-01-12 07:12:04 -0600

    Revert "Work on new homepage."
    This reverts commit cf5d1ee3db42a83dc0b5a24f32b5eebbf1c83e07.

--- r-version/src/App.js
+++ r-version/src/App.js
@@ -1,10 +1,23 @@
-import React from 'react';
-import HomePage from './HomePage'; // Adjust the path if necessary
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <HomePage />
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to refresh.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
