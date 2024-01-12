892be0ef966ce96d50ae7c2069c10c660bf03281
Rich McGhee <rmcghee@stowers.org>
2024-01-12 07:12:03 -0600

    Revert "Styling, links"
    This reverts commit aaa079527007cb8d3855df496164f3395fcb5b50.

--- r-version/src/App.js
+++ r-version/src/App.js
@@ -1,17 +1,11 @@
 import React from 'react';
-import { ThemeProvider } from '@mui/material/styles';
-import CssBaseline from '@mui/material/CssBaseline';
-import theme from './theme';
-import HomePage from './HomePage';
+import HomePage from './HomePage'; // Adjust the path if necessary
 
 function App() {
   return (
-    <ThemeProvider theme={theme}>
-      <CssBaseline />
-      <div className="App">
-        <HomePage />
-      </div>
-    </ThemeProvider>
+    <div className="App">
+      <HomePage />
+    </div>
   );
 }
 
--- r-version/src/HomePage.js
+++ r-version/src/HomePage.js
@@ -1,32 +1,16 @@
 import React from 'react';
-import { Grid, Link, Paper, Container } from '@mui/material';
+import { Link } from '@mui/material';
 
 const HomePage = () => {
   return (
-    <Container>
-      <Paper style={{ padding: 20, marginTop: 30 }}>
-        <Grid container direction="column" alignItems="center" justify="center">
-          <Grid item>
-            <h1>RMcGhee.GitHub.io</h1>
-          </Grid>
-          <Grid item>
-            <Link href="cs_projects.html">CS Projects</Link>
-          </Grid>
-          <Grid item>
-            <Link href="synbio.html">Synthetic Biology</Link><br />
-          </Grid>
-          <Grid item>
-            <Link href="photography.html">Photography</Link><br />
-          </Grid>
-          <Grid item>
-            <Link href="index.html">Home</Link><br />
-          </Grid>
-          <Grid item>
-           <Link href="https://github.com/RMcGhee">My GitHub</Link>
-          </Grid>
-        </Grid>
-      </Paper>
-    </Container>
+    <div>
+      <h1>RMcGhee.GitHub.io</h1>
+      <Link href="cs_projects.html">CS Projects</Link><br />
+      <Link href="synbio.html">Synthetic Biology</Link><br />
+      <Link href="photography.html">Photography</Link><br />
+      <Link href="index.html">Home</Link><br />
+      <Link href="https://github.com/RMcGhee">My GitHub</Link>
+    </div>
   );
 };
 
